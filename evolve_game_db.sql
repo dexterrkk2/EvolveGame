68bd94824e8f7bc269baa11b0178291235e0fe83
dexterrkk2 <95152097+dexterrkk2@users.noreply.github.com>
2025-04-16 20:22:26 -0700

    download sql

--- evolve_game_db.sql
+++ evolve_game_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 01, 2025 at 06:42 PM
+-- Generation Time: Apr 17, 2025 at 05:20 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,18 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `creatortable`
---
-
-CREATE TABLE `creatortable` (
-  `id` int(11) NOT NULL,
-  `PlayerID` int(11) NOT NULL,
-  `CreaturID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `creature`
 --
 
@@ -50,70 +38,19 @@ CREATE TABLE `creature` (
   `Population` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `creaturesgenes`
+-- Dumping data for table `creature`
 --
 
-CREATE TABLE `creaturesgenes` (
-  `id` int(11) NOT NULL,
-  `Creature ID` int(11) NOT NULL,
-  `Gene Id` int(11) NOT NULL,
-  `on/off` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `genes`
---
-
-CREATE TABLE `genes` (
-  `id` int(11) NOT NULL,
-  `name` varchar(10) NOT NULL,
-  `effect` varchar(10) NOT NULL,
-  `Cost` int(11) NOT NULL,
-  `Recessive` tinyint(1) NOT NULL,
-  `PartToEffect` varchar(10) NOT NULL,
-  `GeneType` varchar(10) NOT NULL,
-  `GeneFamily` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `playertable`
---
-
-CREATE TABLE `playertable` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `DisplayName` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `preytable`
---
-
-CREATE TABLE `preytable` (
-  `id` int(11) NOT NULL,
-  `PredatorID` int(11) NOT NULL,
-  `PreyID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `creature` (`id`, `name`, `Damage`, `Health`, `Diet`, `AttackSpeed`, `MoveSpeed`, `Population`) VALUES
+(1, 'bunny', 1, 5, 'plants', 1, 10, 100),
+(2, 'Wolf', 2, 5, 'Meat', 2, 5, 10),
+(3, 'Werewolf', 5, 10, 'Meat', 2, 2, 5),
+(4, 'worm', 1, 1, 'all', 1, 1, 1000);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `creatortable`
---
-ALTER TABLE `creatortable`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `creature`
@@ -122,68 +59,14 @@ ALTER TABLE `creature`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `creaturesgenes`
---
-ALTER TABLE `creaturesgenes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `genes`
---
-ALTER TABLE `genes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `playertable`
---
-ALTER TABLE `playertable`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `preytable`
---
-ALTER TABLE `preytable`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `creatortable`
---
-ALTER TABLE `creatortable`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `creature`
 --
 ALTER TABLE `creature`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `creaturesgenes`
---
-ALTER TABLE `creaturesgenes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `genes`
---
-ALTER TABLE `genes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `playertable`
---
-ALTER TABLE `playertable`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `preytable`
---
-ALTER TABLE `preytable`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
