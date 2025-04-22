e4ec4936a2bbb06cab1c7a2c8c0f127906433e24
dexterrkk2 <95152097+dexterrkk2@users.noreply.github.com>
2025-04-22 11:51:06 -0700

    a

--- EvolveGamePhp/AddGeneToCreature.php
+++ EvolveGamePhp/AddGeneToCreature.php
@@ -1,16 +1,13 @@
 <?php
 require 'ConnectionSettings.php';
 //variables submitted
-//creature name, user id, 
-$creatureName = $_POST["creatureName"];
-$diet = $_POST["CreatureDiet"];
+//creature name, user id,
 $geneID = $_POST["geneID"];
 $creatureID = $_POST["CreaturID"];
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
-$sql = "INSERT INTO creaturesgenes () VALUES ('" .$geneID . "','" . $creatureID ."')";
+$sql = "INSERT INTO creaturesgenes (Creature ID, Gene id) VALUES ('" .$geneID . "','" . $creatureID ."')";
 $result = $conn->query($sql);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
@@ -18,9 +15,10 @@ if ($conn->connect_error) {
 //Show users
 if ($conn->query($sql2) === TRUE) {
     //tell user that name is already taken
-    echo "creature already taken";
+    echo "Gene Added";
     
   } else {
-    echo "creating creature";
+    echo "did not give gene";
+  }
   $conn->close();
 ?>
